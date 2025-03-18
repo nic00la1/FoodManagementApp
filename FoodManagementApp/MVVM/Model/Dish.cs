50df41149484485c87985c2f930f79928248e919
nic00la1 <nic00la.kaleta@gmail.com>
2025-03-18 10:32:35 +0100

    modele potraw i skladnikow

---
+++ FoodManagementApp/MVVM/Model/Dish.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FoodManagementApp.MVVM.Model
+{
+    public class Dish
+    {
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public string Description { get; set; }
+        public string Category { get; set; }
+        public ObservableCollection<Ingredient> Ingredients { get; set; }
+        public string ImageUrl { get; set; }
+    }
+}
---
+++ FoodManagementApp/MVVM/View/MainPage.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace FoodManagementApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+
+}
